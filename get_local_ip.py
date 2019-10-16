441fe3f0e7ee2c6d953ac52d73d041d15b56540f
rootsu <g60432042@gmail.com>
2019-10-16 18:07:51 +0800

    2

--- get_local_ip.py
+++ get_local_ip.py
@@ -1,3 +1,17 @@
 import socket
-localip = socket.gethostbyname(socket.gethostname())
-print(localip)
+
+
+def get_local_ip():
+    localip = socket.gethostbyname(socket.gethostname())
+    return localip
+
+
+def get_local_subnet():
+    ip = get_local_ip()
+    ip_prefix = '_'.join(ip.split('.')[:3])
+    return ip_prefix
+
+
+if __name__ == '__main__':
+    print(get_local_ip())
+    print(get_local_subnet())
