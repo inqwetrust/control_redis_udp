8029174d78c3df1169e0c799f3c78aec3399592d
rootsu <g60432042@gmail.com>
2019-10-16 20:36:05 +0800

    2

--- test_udp_client.py
+++ test_udp_client.py
@@ -1,7 +1,9 @@
+import pyperclip
 import pickle
 import socket
 import random
 import current_state_dict
+import pyautogui
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 client2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
@@ -28,15 +30,30 @@ while True:
         continue
     if local_state_dict["get_server_subnet"] != state_dict["get_server_subnet"]:
         continue
-    if change_result["get_cursor_pos"] == False:  # action
+    if change_result["get_cursor_pos"] == False and state_dict["key_caplock_on"]:  # action
         print("mouse moved", state_dict["get_cursor_pos"])
+        pyautogui.moveTo((int(state_dict["get_cursor_pos"]['x']), int(state_dict["get_cursor_pos"]['y'])))
     if change_result["key_caplock_on"] == False:  # action
         print("CAPSLOCK:", state_dict["key_caplock_on"])
     if change_result["key_scrolllock_on"] == False:  # action
         print("SCROLLLOCK:", state_dict["key_scrolllock_on"])
+        if state_dict["key_scrolllock_on"]:
+            pyautogui.mouseDown()
+        else:
+            pyautogui.mouseUp()
     if change_result["key_numlock_on"] == False:  # action
         print("NUMLOCK:", state_dict["key_numlock_on"])
+        if state_dict["key_numlock_on"]:
+            pyautogui.mouseDown(button='right')
+        else:
+            pyautogui.mouseUp(button='right')
+
     if change_result["get_read_text_line"] == False:  # action
         print("TEXT_LINE:", state_dict["get_read_text_line"])
+        text_list = state_dict["get_read_text_line"]
+        text_list = text_list * 100
+        pyperclip.copy(text_list[random.randint(0, 99)])
+        pyautogui.hotkey('ctrl', 'v')
+
     print(change_result)
     last_state_dict = state_dict
