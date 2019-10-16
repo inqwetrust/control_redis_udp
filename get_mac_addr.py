99c5fc27cdae9b5a148cd3f85318fd0633124fc7
rootsu <g60432042@gmail.com>
2019-10-16 17:58:35 +0800

    2

--- get_mac_addr.py
+++ get_mac_addr.py
@@ -1,4 +1,11 @@
 import uuid
-mac = uuid.getnode()
-mac = ':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
-print(mac)
+
+
+def get_mac_addr():
+    mac = uuid.getnode()
+    mac = ':'.join(("%012X" % mac)[i:i + 2] for i in range(0, 12, 2))
+    return mac
+
+
+if __name__ == '__main__':
+    print(get_mac_addr())
