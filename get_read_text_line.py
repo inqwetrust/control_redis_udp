c5ce33c1b253a9c765cfd72892926f70a208f7aa
rootsu <g60432042@gmail.com>
2019-10-16 18:11:07 +0800

    2

--- get_read_text_line.py
+++ get_read_text_line.py
@@ -1,6 +1,10 @@
-f = open('text_list.txt', 'r')
-text_list = [t.replace("\n", "") for t in f.readlines()]
-text_list = text_list * 200
-f.close()
+def get_read_text_line():
+    f = open('text_list.txt', 'r')
+    text_list = [t.replace("\n", "") for t in f.readlines()]
+    text_list = text_list * 200
+    f.close()
+    return text_list
 
-print(text_list)
+
+if __name__ == '__main__':
+    print(get_read_text_line())
