0a09629e29a786cbd072fa5160319602b835a63c
User <g60432042@gmail.com>
2019-10-17 12:51:44 +0800

    v2

---
+++ get_ram_info.py
@@ -0,0 +1,28 @@
+import os
+from ctypes import *
+from ctypes.wintypes import *
+
+from subprocess import check_output
+
+class MEMORYSTATUS(Structure):
+    _fields_ = [
+        ('dwLength', DWORD),
+        ('dwMemoryLoad', DWORD),
+        ('dwTotalPhys', DWORD),
+        ('dwAvailPhys', DWORD),
+        ('dwTotalPageFile', DWORD),
+        ('dwAvailPageFile', DWORD),
+        ('dwTotalVirtual', DWORD),
+        ('dwAvailVirtual', DWORD),
+    ]
+
+
+def winmem():
+    x = MEMORYSTATUS()
+    windll.kernel32.GlobalMemoryStatus(byref(x))
+    return x
+
+
+if __name__ == '__main__':
+    print(winmem().dwTotalPhys)
+    print(os.system("wmic memorychip get /VALUE |findstr \"Capacity\""))
