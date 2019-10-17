d02edb4cf6c8dbcad0dab73eedf9947f2932fdf1
User <g60432042@gmail.com>
2019-10-17 15:32:29 +0800

    v2

--- get_cpu_model.py
+++ get_cpu_model.py
@@ -1,8 +1,19 @@
-import cpuinfo
+import subprocess
 
 
 def get_cpu_brand():
-    return cpuinfo.get_cpu_info()["brand"]
+    command = "wmic cpu get name"
+    pipe = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = ""
+    while True:
+        line = pipe.stdout.readline()
+        if line:
+            result += str(line)
+        if not line:
+            break
+    result = result.strip()
+    result = result[54:-18]
+    return result
 
 
 if __name__ == '__main__':
