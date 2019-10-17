30c0e86024cfa86500af560f27080587fef4b5ca
User <g60432042@gmail.com>
2019-10-17 12:58:38 +0800

    v2

--- get_cpu_model.py
+++ get_cpu_model.py
@@ -1,2 +1,9 @@
 import cpuinfo
-print(cpuinfo.get_cpu_info()["brand"])
+
+
+def get_cpu_brand():
+    return cpuinfo.get_cpu_info()["brand"]
+
+
+if __name__ == '__main__':
+    print(get_cpu_brand())
