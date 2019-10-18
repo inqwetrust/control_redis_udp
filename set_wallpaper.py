77dcf4a62eb9f03d79c7ba345739acc80f3ecb42
User <g60432042@gmail.com>
2019-10-18 10:21:11 +0800

    v2

--- set_wallpaper.py
+++ set_wallpaper.py
@@ -50,4 +50,7 @@ if __name__ == '__main__':
     info_list.append(local_state_dict["get_mac_addr"])
     info_list.append(local_state_dict["get_server_ip"])
     info_list.append(local_state_dict["display"])
+    info_list = ['{}'.format(i) for i in info_list]
+    info_list = "|".join(info_list)
+    print(info_list)
     set_wallpaper(info_list)
