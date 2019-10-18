733abaf34dfd14b173dedc575789875271898eac
rootsu <g60432042@gmail.com>
2019-10-18 08:46:46 +0800

    2

--- remote_zombie.py
+++ remote_zombie.py
@@ -9,20 +9,20 @@ import pyautogui
 from multiprocessing import Process
 
 pyautogui.FAILSAFE = False
+# client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+client2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+
+# client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+client2.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+# client.bind(("", 37020))
+client2.bind(("", 37021))
+last_state_dict = current_state_dict.get_current_state_dict()
+local_state_dict = last_state_dict
+print(local_state_dict)
 
 
 def zombie():
-    # client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-    client2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-
-    # client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    client2.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-    # client.bind(("", 37020))
-    client2.bind(("", 37021))
-    last_state_dict = current_state_dict.get_current_state_dict()
-    local_state_dict = last_state_dict
-    print(local_state_dict)
     while True:
         # hash = random.randint(0, 100)
         try:
