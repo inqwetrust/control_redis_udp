cca61c196a575a62ff8eebcb34210ee915ccc524
rootsu <g60432042@gmail.com>
2019-10-21 09:10:08 +0800

    vv2

--- get_mouse_click.py
+++ get_mouse_click.py
@@ -3,17 +3,17 @@ import win32api
 import time
 
 
-def get_left_click():
+def get_click_state():
     state_left = win32api.GetKeyState(0x01)  # Left button down = 0 or 1. Button up = -127 or -128
     state_right = win32api.GetKeyState(0x02)  # Right button down = 0 or 1. Button up = -127 or -128
     return state_left, state_right
 
 
 if __name__ == '__main__':
-    last_state = get_left_click()
+    last_state = get_click_state()
     while True:
-        state = get_left_click()
+        state = get_click_state()
         if state != last_state:
-            print(get_left_click())
+            print(get_click_state())
         # time.sleep(1)
         last_state = state
