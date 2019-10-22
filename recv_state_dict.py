9be8de5b3999f6f6b831a92677506ee57f5b3b8a
User <g60432042@gmail.com>
2019-10-22 09:30:34 +0800

    vv2

--- recv_state_dict.py
+++ recv_state_dict.py
@@ -1,15 +1,19 @@
 import pickle
 import socket
 import traceback
+import pandas as pd
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 client.bind(("", 37020))
+collected_state_dict = {}
 while True:
     try:
         data, addr = client.recvfrom(10240)
         state_dict = pickle.loads(data)
-
+        collected_state_dict[state_dict["get_server_ip"]] = state_dict
+        df = pd.DataFrame(collected_state_dict)
+        df.to_html('out.html')
         print("received message:{}".format(state_dict))
     except:
         print(traceback.format_exc())
