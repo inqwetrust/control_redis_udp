833e9e3191ad8dd53efc94e47451f646642e076f
User <g60432042@gmail.com>
2019-10-22 09:15:49 +0800

    vv2

--- remote_controller.py
+++ remote_controller.py
@@ -5,15 +5,10 @@ import current_state_dict
 import time
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-
 server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
 # Set a timeout so the socket does not block
-
 # indefinitely when trying to receive data.
-
 server.settimeout(0.2)
-
 server.bind(("", 44444))
 
 message = b"your very important message" * 1
