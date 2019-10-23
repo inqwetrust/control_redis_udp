04f8f6d6540483e03fa6843c0d64f7ac241ec136
User <g60432042@gmail.com>
2019-10-23 15:37:32 +0800

    vv2

--- recv_state_dict.py
+++ recv_state_dict.py
@@ -16,6 +16,17 @@ while True:
         collected_state_dict[state_dict["get_server_ip"]] = state_dict
         collected_state_list = [v for k, v in collected_state_dict.items()]
         df = DataFrame(collected_state_list)
+        df = df.drop('get_uuid', 1)
+        df = df.drop('get_cursor_pos', 1)
+        df = df.drop('get_server_subnet', 1)
+        df = df.drop('key_caplock_on', 1)
+        df = df.drop('key_caplock_off', 1)
+        df = df.drop('key_scrolllock_on', 1)
+        df = df.drop('key_scrolllock_off', 1)
+        df = df.drop('key_numlock_on', 1)
+        df = df.drop('key_numlock_off', 1)
+        df = df.drop('get_read_text_line', 1)
+        df = df.drop('start_time', 1)
         df.to_html('status_report.html')
         print("received message:{}".format(state_dict))
     except:
