4dc303ba69c4c89842cc5d234f4de08d90889981
rootsu <g60432042@gmail.com>
2019-10-25 16:00:25 +0800

    vv2

--- batch_pyinstaller_package.bat
+++ batch_pyinstaller_package.bat
@@ -1,6 +1,8 @@
 del *.spec
+rmdir /s /q dist
+rmdir /s /q build
 pyinstaller -F remote_controller.py
-pyinstaller -F remote_zombie.py
+pyinstaller -F --hidden-import -w remote_zombie.py
 pyinstaller -F set_wallpaper.py
 pyinstaller -F recv_state_dict.py
 copy text_list.txt dist\
