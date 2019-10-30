6fb1324b11f9d807642157f48c63e054908eb518
User <g60432042@gmail.com>
2019-10-30 19:19:21 +0800

    vv2

--- get_mac_csv.py
+++ get_mac_csv.py
@@ -1,18 +1,28 @@
 import csv
 
+mac_dict = {}
 
-def get_mac_csv():
-    mac_dict = {}
-    filename = 'room1_mac.csv'
+
+def read_file(filename):
     with open(filename, 'r') as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
             mac = str(row[0])
             mac = mac.replace('-', '')
-            print(mac)
-            mac = ':'.join([mac[i:i+2] for i,j in enumerate(mac) if not (i%2)])
+            mac = ':'.join([mac[i:i + 2] for i, j in enumerate(mac) if not (i % 2)])
             mac = mac.upper()
-            mac_dict[mac] = {"port": row[1], "room": filename}
+            port = row[1]
+            port = port.split('/')
+            port = port[-1]
+            mac_dict[mac] = {"port": port, "room": filename[:5]}
+    pass
+
+
+def get_mac_csv():
+    filename = 'room1_mac.csv'
+    read_file(filename)
+    filename = 'room2_mac.csv'
+    read_file(filename)
     return mac_dict
 
 
